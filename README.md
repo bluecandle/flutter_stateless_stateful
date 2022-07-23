dab2d0a7f0f492a2ff88364480ada1bde6d9df6a
bluecandle <jinyeongkim37@gmail.com>
2022-07-23 16:35:14 +0900

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# 강의 4
+[링크](https://www.youtube.com/watch?v=a7sbSfVbeF8)
---
+++ lib/stateless_sample_widget.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class StatelessSampleWidget extends StatelessWidget {
+  const StatelessSampleWidget({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
